7b1b8d0eb5bced92e59c9857fd78399979ecd42b
taherbadri <badri.taher@yahoo.com>
2024-04-25 11:56:07 +0530

    finished basic structure of navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
+
+	daisyui: {
+		themes: [
+			{
+				light: {
+					...require("daisyui/src/theming/themes")["light"],
+					primary: "#02393e",
+					secondary: "#035962",
+					accent: "#ff6347",
+					neutral: "f0f0f0",
+				},
+			},
+			{
+				dark: {
+					...require("daisyui/src/theming/themes")["dark"],
+					primary: "#02393e",
+					secondary: "#035962",
+					accent: "#ff6347",
+					neutral: "#121212",
+				},
+			},
+		],
+	},
+
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+		},
 	},
 	plugins: [
 		require("@tailwindcss/forms"),
